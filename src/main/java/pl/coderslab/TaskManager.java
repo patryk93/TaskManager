342b3cbe9747fa7e29416eaf1f16758e70918051
patryk93 <patryk.r.skubisz@gmail.com>
2022-11-23 20:28:09 +0100

    show options

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -1,4 +1,28 @@
 package pl.coderslab;
 
+import java.util.Scanner;
+
 public class TaskManager {
+
+    static String[][] tasks;
+    public static void main(String[] args) {
+        showOptions();
+        getInfo();
+    }
+
+    public static void showOptions() {
+        System.out.println(ConsoleColors.BLUE + "Please select an option: ");
+        System.out.println(ConsoleColors.RESET + "add");
+        System.out.println("remove");
+        System.out.println("list");
+        System.out.println("exit");
+    }
+
+    public static void getInfo() {
+        Scanner scann = new Scanner(System.in);
+
+    }
+
 }
+
+
